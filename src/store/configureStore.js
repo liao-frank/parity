e80f46e1ffc6972e407e6ad74e73b4bdcd95230b
Frank Liao <fliao@squarespace.com>
2018-08-01 11:31:39 -0400

    Logger middleware based on NODE_ENV

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,16 +2,25 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
-
 const loggerMiddleware = createLogger();
+
+let middleware;
+if (process.env.NODE_ENV === 'development') {
+  middleware = applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  );
+}
+else {
+  middleware = applyMiddleware(
+    thunkMiddleware
+  );
+}
 
 export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
-    )
+    middleware
   );
 };
